a36302edff7c5c3857d2b9d3759feb63151eb2e6
mikhailperevalov <perevalov99@gmail.com>
2022-02-08 20:20:00 +0300

    sprint 4.2

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -6,15 +6,17 @@ RequestQueue::RequestQueue(const SearchServer& search_server)
         , current_time_(0) {} 
 
 std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) { 
-    const auto result = search_server_.FindTopDocuments(raw_query, status); 
-    AddRequest(result.size()); 
-    return result; 
+    return (AddFindRequest(raw_query,
+                             [status] (int document_id, DocumentStatus document_status, int rating)
+                             {
+                                return status == document_status;
+                             }
+                          )
+           ); 
 } 
 
 std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) { 
-    const auto result = search_server_.FindTopDocuments(raw_query); 
-    AddRequest(result.size()); 
-    return result; 
+    return (AddFindRequest(raw_query, DocumentStatus::ACTUAL));
 } 
 
 int RequestQueue::GetNoResultRequests() const { 
